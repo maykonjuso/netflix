b4a95dd350b995cef6a46255e950870374c3695c
maykonjuso <maykola1331@gmail.com>
2022-02-13 21:31:59 -0300

    starting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
+import './App.css';
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
 const App = () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () =>{
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      console.log(chosen);
+      
+    }
+    loadAll();
+  }, []);
+  
   return(
-  <div>
-    hello world
-  </div>
+    <div classname="page">
+
+      {featuredData &&
+      <FeaturedMovie item={featuredData} />
+      }
+
+      <section className='lists'>
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+    </div>
   ) 
 };
 
---
+++ my-app/src/components/FeaturedMovie.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './FeaturedMovie.css';
+
+export default () => {
+    return (
+        <div>
+            FILME EM DESTAQUE
+        </div>
+    );
+}
